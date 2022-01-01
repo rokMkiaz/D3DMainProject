8e1a1b90bb71d6dc0400807406a5619e3293a300
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2022-01-01 14:16:48 +0900

    초기 커밋.

---
+++ ModelEditor/ExportFile.cpp
@@ -0,0 +1,87 @@
+#include"stdafx.h"
+#include"ExportFile.h"
+#include"Writer/Converter.h"
+
+void ExportFile::Initialize()
+{
+
+	Airplane();
+	Tower();
+	Tank();
+    Kachujin();
+	IronMan();
+}
+
+void ExportFile::Airplane()
+{
+	Converter* conv = new Converter();
+	conv->ReadFile(L"B787/Airplane.fbx");
+	conv->ExportMesh(L"B787/Airplane");
+	conv->ExportMaterial(L"B787/Airplane");
+	SafeDelete(conv);
+}
+
+void ExportFile::Tower()
+{
+	Converter* conv = new Converter();
+	conv->ReadFile(L"Tower/Tower.fbx");
+	conv->ExportMesh(L"Tower/Tower");
+	conv->ExportMaterial(L"Tower/Tower");
+	SafeDelete(conv);
+}
+
+void ExportFile::Tank()
+{
+	Converter* conv = new Converter();
+	conv->ReadFile(L"Tank/Tank.fbx");
+	conv->ExportMesh(L"Tank/Tank");
+	conv->ExportMaterial(L"Tank/Tank",false); //xml수정후 수정불가하게
+	SafeDelete(conv);
+}
+
+void ExportFile::Kachujin()
+{
+	Converter* conv = NULL;
+
+	conv = new Converter();
+	conv->ReadFile(L"Kachujin/Mesh.fbx");
+	conv->ExportMesh(L"Kachujin/Mesh");
+	conv->ExportMaterial(L"Kachujin/Mesh");
+	SafeDelete(conv);
+
+	
+	conv = new Converter();
+	conv->ReadFile(L"Kachujin/Sword And Shield Idle.fbx");
+	conv->ExportAnimClip(0, L"Kachujin/Sword And Shield Idle");
+	SafeDelete(conv);
+
+	conv = new Converter();
+	conv->ReadFile(L"Kachujin/Sword And Shield Run.fbx");
+	conv->ExportAnimClip(0,L"Kachujin/Sword And Shield Run");
+	SafeDelete(conv);
+	 
+	conv = new Converter();
+	conv->ReadFile(L"Kachujin/Sword And Shield Slash.fbx");
+	conv->ExportAnimClip(0,L"Kachujin/Sword And Shield Slash");
+	SafeDelete(conv);
+	 
+	conv = new Converter();
+	conv->ReadFile(L"Kachujin/Salsa Dancing.fbx");
+	conv->ExportAnimClip(0,L"Kachujin/Salsa Dancing");
+	SafeDelete(conv);
+
+	
+	//vector<wstring> clipNames;
+	//conv->ClipList(&clipNames);
+}
+
+void ExportFile::IronMan()
+{
+	Converter* conv = new Converter();
+	conv->ReadFile(L"IronMan/IronMan.fbx");
+	conv->ExportMesh(L"IronMan/IronMan");
+	conv->ExportMaterial(L"IronMan/IronMan");
+	SafeDelete(conv);
+}
+
+
---
+++ ModelEditor/ExportFile.h
@@ -0,0 +1,22 @@
+#pragma once
+#include"Systems\IExecute.h"
+class ExportFile : public IExecute
+{
+public:
+	void Initialize() override;
+	void Ready() override {};
+	void Destroy() override {};
+	void Update() override {};
+	void PreRender() override {};
+	void Render() override {};
+	void PostRender() override {};
+	void ResizeScreen() override {};
+
+private:
+	void Airplane();
+	void Tower();
+	void Tank();
+	void Kachujin();
+	void IronMan();
+
+};
